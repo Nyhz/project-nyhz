92a450ad089906b6c1777af28716fdad604b5bc1
Daniel Calvo <daniel.calvo@tailor-hub.com>
2022-06-11 23:35:03 +0200

    Initialized project

---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+import { Text } from "@chakra-ui/react";
+
+export const Hero = () => {
+  return (
+    <div>
+      <Text fontFamily={"SourceCodePro-Bold"}>Hola soy Dany!</Text>
+    </div>
+  );
+};
